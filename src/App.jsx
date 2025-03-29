2ba066d6ae40da7b67c5c9d5263df3880c7ebb94
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-03-28 19:31:49 -0700

    completed lab

--- src/App.jsx
+++ src/App.jsx
@@ -35,81 +35,51 @@ function App() {
       strength: 7,
       agility: 6,
       img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png',
-    },
-    {
-      id: 5,
-      name: 'Sharpshooter',
-      price: 20,
-      strength: 6,
-      agility: 8,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png',
-    },
-    {
-      id: 6,
-      name: 'Medic',
-      price: 15,
-      strength: 5,
-      agility: 7,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png',
-    },
-    {
-      id: 7,
-      name: 'Engineer',
-      price: 16,
-      strength: 6,
-      agility: 5,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png',
-    },
-    {
-      id: 8,
-      name: 'Brawler',
-      price: 11,
-      strength: 8,
-      agility: 3,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png',
-    },
-    {
-      id: 9,
-      name: 'Infiltrator',
-      price: 17,
-      strength: 5,
-      agility: 9,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png',
-    },
-    {
-      id: 10,
-      name: 'Leader',
-      price: 22,
-      strength: 7,
-      agility: 6,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png',
-    },
-  ]
-)
+    }
+  ]);
 
-const addToTeam = (fighter) => {
-  setTeam(function (prevTeam) {
-    const newTeam = prevTeam.concat(fighter); // Using .concat() instead of the spread operator
-    return newTeam;
-  });
-};
+  const addToTeam = (fighter) => {
+    setTeam((prevTeam) => [...prevTeam, fighter]);
+    setMoney((prevMoney) => prevMoney - fighter.price);
+  };
 
-return (
-  <div>
-    <h2>Zombie Fighters</h2>
-    <ul>
-      {zombieFighters.map((fighter) => (
-        <li key={fighter.id}>
-          <h3>{fighter.name}</h3>
-          <img src={fighter.img} alt={fighter.name} width="100" />
-          <p>Price: ${fighter.price}</p>
-          <p>Strength: {fighter.strength}</p>
-          <p>Agility: {fighter.agility}</p>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+  const handleRemoveFighter = (fighter) => {
+    setTeam((prevTeam) => prevTeam.filter(f => f.id !== fighter.id));
+    setZombieFighters((prevFighters) => [...prevFighters, fighter]);
+    setMoney((prevMoney) => prevMoney + fighter.price);
+  };
+
+  return (
+    <div>
+      <h2>Zombie Fighters</h2>
+      <ul>
+        {zombieFighters.map((fighter) => (
+          <li key={fighter.id}>
+            <h3>{fighter.name}</h3>
+            <img src={fighter.img} alt={fighter.name} width="100" />
+            <p>Price: ${fighter.price}</p>
+            <p>Strength: {fighter.strength}</p>
+            <p>Agility: {fighter.agility}</p>
+            <button onClick={() => addToTeam(fighter)}>Add</button>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Your Team</h2>
+      <ul>
+        {team.map((fighter) => (
+          <li key={fighter.id}>
+            <h3>{fighter.name}</h3>
+            <img src={fighter.img} alt={fighter.name} width="100" />
+            <p>Strength: {fighter.strength}</p>
+            <p>Agility: {fighter.agility}</p>
+            <button onClick={() => handleRemoveFighter(fighter)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <h2>Money: ${money}</h2>
+    </div>
+  );
 }
 
 export default App;
